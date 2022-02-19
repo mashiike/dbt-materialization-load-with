b8907fbbd2bd17181ca71a2c51581750c02d44c7
mashiike <ikeda-masashi@kayac.com>
2022-02-19 16:47:44 +0900

    extend is_incremental

--- macros/default/is_incremental.sql
+++ macros/default/is_incremental.sql
@@ -6,7 +6,7 @@
   -- The code license for the original dbt-core macro 'is_incremental' is as follows
   -- https://github.com/dbt-labs/dbt-core/blob/v1.0.2/License.md
 -#}
-{% macro is_incremental() %}
+{% macro is_load_incremental() %}
     {#-- do not run introspective queries in parsing #}
     {% if not execute %}
         {{ return(False) }}
@@ -14,7 +14,11 @@
         {% set relation = adapter.get_relation(this.database, this.schema, this.table) %}
         {{ return(relation is not none
                   and relation.type == 'table'
-                  and model.config.materialized == 'incremental'
+                  and model.config.materialized == 'load_incremental'
                   and not should_full_refresh()) }}
     {% endif %}
+{% endmacro %}
+
+{% macro is_incremental() %}
+    {{ return(dbt.is_incremental() or materialization_load_with.is_load_incremental()) }}
 {% endmacro %}
