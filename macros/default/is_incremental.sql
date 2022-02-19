97d2bb227080282f5d6810fcdcb7cc9c4db121c7
mashiike <ikeda-masashi@kayac.com>
2022-02-19 16:20:13 +0900

    copy code is_incremental from dbt-core v1.0.2

---
+++ macros/default/is_incremental.sql
@@ -0,0 +1,20 @@
+{#-
+  -- Basically, it is implemented by referring to the dbt-core macro 'is_incremental' process. (dbt version 1.0.2)
+  -- The original macro 'is_incremental' code is shown below.
+  -- https://github.com/dbt-labs/dbt-core/blob/v1.0.2/core/dbt/include/global_project/macros/materializations/models/incremental/is_incremental.sql
+  --
+  -- The code license for the original dbt-core macro 'is_incremental' is as follows
+  -- https://github.com/dbt-labs/dbt-core/blob/v1.0.2/License.md
+-#}
+{% macro is_incremental() %}
+    {#-- do not run introspective queries in parsing #}
+    {% if not execute %}
+        {{ return(False) }}
+    {% else %}
+        {% set relation = adapter.get_relation(this.database, this.schema, this.table) %}
+        {{ return(relation is not none
+                  and relation.type == 'table'
+                  and model.config.materialized == 'incremental'
+                  and not should_full_refresh()) }}
+    {% endif %}
+{% endmacro %}
